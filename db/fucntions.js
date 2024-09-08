00f2ffa1f1b652a8d8728a3f5bf17b72f5bcc97d
Aman <aman.prajapati7022@gmail.com>
2024-09-09 00:14:59 +0530

    redesign /game page with game-img as bg for div

--- db/fucntions.js
+++ db/fucntions.js
@@ -6,7 +6,7 @@ const pool = new Pool({connectionString: process.env.CONNECTION_STRING});
 
 
 async function getGames() {
-    const {rows} = await pool.query('SELECT id, name, logo_img_url from items');
+    const {rows} = await pool.query('SELECT id, name, logo_img_url,cover_img_url  from items');
     return rows;
 }
 
